d0b38480aa0be58204ae85c882ca33d7584d98a3
Nicolas Cena <nicolasnahuelcena@gmail.com>
2024-06-22 11:58:43 +0200

    [FEAT] Implementamos uso diferenciado del componente description

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,8 +1,7 @@
 import useTasks from "../hooks/useTasks";
 import { Error } from "../components/Error";
-import { TaskCard } from "../components/Task/TaskCard";
+import { TaskCard } from "../components/Task";
 import TaskButton from "../components/Task/TaskButton";
-import TaskDescription from "../components/Task/TaskDescription";
 
 const Tasks = () => {
   const { loading, tasks, error } = useTasks();
@@ -15,7 +14,7 @@ const Tasks = () => {
       {Array.isArray(tasks) && tasks.length > 0 && tasks.map((task, index) => (
         <TaskCard key={index} task={task} >
           <TaskButton onClick={() => {}} buttonText="Enviar" />
-          <TaskDescription  text="Prueba descripcion"/>
+          <TaskCard.Description  text="Prueba descripcion"/>
         </TaskCard>
       ))}
     </div>
